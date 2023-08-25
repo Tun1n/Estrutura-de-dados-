f277c985974cc03f8b698594f297b806d032d334
Tun1n <slpedro21@outlook.com>
2023-08-25 19:31:32 -0300

    Exercício 11

---
+++ Exercício 11/main.js
@@ -0,0 +1,16 @@
+function calcular() {
+    let a = Number(document.getElementById('valor1').value);
+    document.getElementById("a").innerHTML = "O valor de a é: " + a;
+
+    let b = Number(document.getElementById('valor1').value);
+    document.getElementById("b").innerHTML = "O valor de b é: " + b;
+
+    let c = Number(document.getElementById('valor3').value);
+    document.getElementById("c").innerHTML = "O valor de c é: " + c;
+
+    let resultado = a**2 + c**2 + b*(2*(a+b+c));
+    document.getElementById("resultado").innerHTML = "A solução é: " + resultado;
+
+
+}
+    
