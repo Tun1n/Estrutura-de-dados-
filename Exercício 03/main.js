ab2b5a6d890fc0d2f5ea899c29d67e4774d9de06
Tun1n <113259985+Tun1n@users.noreply.github.com>
2023-08-25 00:58:21 -0300

    Update main.js

--- Exercício 03/main.js
+++ Exercício 03/main.js
@@ -1,10 +1,8 @@
 function Elevar() {
-    // Variável Global
      let Número1 = Number(document.getElementById('Número1').value);
      Número1 = parseInt(Número1);
      let Resultado = (Número1 ** 2) ;
      document.getElementById("Resultado").innerHTML = "O resultado é " + Resultado;
-     //console.log("O resultado é: ", Resultado);
     }
 
 
