65f1d006cad7d63edca36e97117b05a43fe99a22
Tun1n <113259985+Tun1n@users.noreply.github.com>
2023-08-25 00:59:15 -0300

    Update main.js

--- Exercício 1 do megamente/main.js
+++ Exercício 1 do megamente/main.js
@@ -1,9 +1,7 @@
 function Calcular() {
-// Variável Global
  let Número1 = Number(document.getElementById('Número1').value);
  let Número2 = Number(document.getElementById('Número2').value);
  let Numero3 = Number(document.getElementById('Número3').value);
  let Resultado = (Número1 + Número2) * Numero3;
  document.getElementById("Resultado").innerHTML = "O resultado é " + Resultado;
- //console.log("O resultado é: ", Resultado);
 }
