d0bf64cff83864da7a9a2b5c013c2fd986401f9c
Tun1n <113259985+Tun1n@users.noreply.github.com>
2023-08-25 00:58:55 -0300

    Update main.js

--- Exercício 02/main.js
+++ Exercício 02/main.js
@@ -1,45 +1,35 @@
 function Soma() {
-    // Variável Global
      let Número1 = Number(document.getElementById('Número1').value);
      let Número2 = Number(document.getElementById('Número2').value);
      let Resultado = (Número1 + Número2) ;
      document.getElementById("Resultado").innerHTML = "O resultado é " + Resultado;
-     //console.log("O resultado é: ", Resultado);
     }
 
 function Subtração() {
-        // Variável Global
          let Número1 = Number(document.getElementById('Número1').value);
          let Número2 = Number(document.getElementById('Número2').value);
          let Resultado = (Número1 - Número2) ;
          document.getElementById("Resultado").innerHTML = "O resultado é " + Resultado;
-         //console.log("O resultado é: ", Resultado);
     }
 
     
 function Potência2() {
-    // Variável Global
      let Número1 = Number(document.getElementById('Número1').value);
      let Resultado = (Número1**2);
      document.getElementById("Resultado").innerHTML = "O resultado é " + Resultado;
-     //console.log("O resultado é: ", Resultado);
 }
 
 
 function Divisão() {
-    // Variável Global
      let Número1 = Number(document.getElementById('Número1').value);
      let Número2 = Number(document.getElementById('Número2').value);
      let Resultado = (Número1/Número2) ;
      document.getElementById("Resultado").innerHTML = "O resultado é " + Resultado;
-     //console.log("O resultado é: ", Resultado);
 }
 
 function Multiplicação() {
-    // Variável Global
      let Número1 = Number(document.getElementById('Número1').value);
      let Número2 = Number(document.getElementById('Número2').value);
      let Resultado = (Número1*Número2) ;
      document.getElementById("Resultado").innerHTML = "O resultado é " + Resultado;
-     //console.log("O resultado é: ", Resultado);
 }
