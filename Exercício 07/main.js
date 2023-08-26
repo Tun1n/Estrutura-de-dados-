151a0c2202105cb67b2670685072d2cb2fb89a5e
Tun1n <slpedro21@outlook.com>
2023-08-26 16:44:44 -0300

    Exercícios

--- Exercício 07/main.js
+++ Exercício 07/main.js
@@ -1,6 +1,7 @@
 function preçofinal() {
     let Valorfábrica = Number(document.getElementById('Número1').value);
     let Preçototal = Valorfábrica + ((45/100) * Valorfábrica) + ((25/100) * Valorfábrica);
+
     document.getElementById("Preçototal").innerHTML = "O preço total do veículo é : " + Preçototal;
 }
 
