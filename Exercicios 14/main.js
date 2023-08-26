3d6cbe5ca38f6c89ee88c236fcf4a3d4dede2e96
Tun1n <slpedro21@outlook.com>
2023-08-26 00:21:18 -0300

    Exercício 14

---
+++ Exercicios 14/main.js
@@ -0,0 +1,10 @@
+function trocar() {
+    let a = Number(document.getElementById('valor2').value);
+    document.getElementById("a").innerHTML = "O valor de a é: " + a;
+
+    let b = Number(document.getElementById('valor1').value);
+    document.getElementById("b").innerHTML = "O valor de b é: " + b;
+
+
+    
+}
